64b03eda5cc4b19d9c2c7eb2d45c2d1d5c3c4abf
webdev-vs <vladimirgdsr@gmail.com>
2019-05-29 14:36:10 -0700

    added theme with bootstrap

---
+++ enqueuing_bootstrap/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>WP with Bootstrap</title>
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+  <header>
+    <section class="top-bar">
+      <div class="container">
+        <div class="row">
+          <div class="social-media-icons col-xl-10 col-md-8 col-sm-8 col-6">Social Icons</div>
+          <div class="search col-xl-2 col-md-4 col-sm-4 col-6 text-right">Search</div>
+        </div>
+      </div>      
+    </section>
+    <section class="menu-area">
+      <section class="logo">Logo</section>
+      <nav class="main-menu">Menu</nav>
+    </section>
+  </header>
